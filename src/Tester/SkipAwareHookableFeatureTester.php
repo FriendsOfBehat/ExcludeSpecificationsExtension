4099da7c4e6108f666d73c7e435cee936977c49a
Kibsoon <Kibsoon@users.noreply.github.com>
2016-10-27 10:55:58 +0200

    Add copyright holder to license notification
    * Update SkipAwareHookableFeatureTester.php
    
    Please keep the author. Remember about copywright!
    
    * Update SkipAwareHookableFeatureTester.php

--- src/Tester/SkipAwareHookableFeatureTester.php
+++ src/Tester/SkipAwareHookableFeatureTester.php
@@ -3,6 +3,8 @@
 /*
  * This file is part of the SkipExtension package.
  *
+ * (c) Kamil Kokot <kamil@kokot.me>
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
